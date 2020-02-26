5c06008ecbe29200a5f3e9d7b430eb28f72f5cf5
raghuthakur <44355846+raghuthakur@users.noreply.github.com>
2020-02-26 11:22:06 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -16,14 +16,14 @@ let config = {
         "EMAIL_TLS": true
     },
     "server": {
-        "NODE_SERVER": "http://13.232.62.239",
+        "NODE_SERVER": "",
         "ROOT_DIR": "/var/www/html/",
         "BASE_URL": "http://13.232.62.239/new_node_structure/",
         "NODE_SERVER_PORT": "6565",
         "MYSQL_HOST": 'localhost',
-        "MYSQL_USER": 'solync_db',
-        "MYSQL_PASSWORD": 'ksjk29dn29',
-        'MYSQL_DATABASE': 'solync_db',
+        "MYSQL_USER": '',
+        "MYSQL_PASSWORD": '',
+        'MYSQL_DATABASE': '',
         "EMAIL_USER": 'tecugopm@gmail.com',
         "EMAIL_PASS": 'Techugo@123',
         "EMAIL_HOST": 'smtp.gmail.com',
